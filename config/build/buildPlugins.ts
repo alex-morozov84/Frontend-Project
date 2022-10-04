544124f605b9c7c91ba3de6eb1483729d6f78310
Alexander Morozov <alex.morozov84@yandex.ru>
2022-10-04 22:23:07 +0300

    bundle analyzer isDev

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,7 +6,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -18,10 +18,15 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    ...(isDev ? [new webpack.HotModuleReplacementPlugin()] : []),
-    ...(isDev ? [new ReactRefreshWebpackPlugin()] : []),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
+  }
+
+  return plugins;
 }
