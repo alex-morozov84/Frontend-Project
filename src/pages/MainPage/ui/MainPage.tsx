88bb8f5f8876b2d8237064df35d57ad2e1b1e4ea
Alexander Morozov <alex.morozov84@yandex.ru>
2022-10-30 18:59:21 +0300

    fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
-  const [value, setValue] = useState('');
-
-  const onChange = (val: string) => {
-    setValue(val);
-  };
 
   return (
     <div>
