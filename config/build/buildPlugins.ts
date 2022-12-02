3fbb59f021585d6ba94af8ea91c0866c684f2301
Alexander Morozov <alex.morozov84@yandex.ru>
2022-12-02 20:06:29 +0300

    add circular dependency plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -27,6 +28,10 @@ export function buildPlugins({
       patterns: [
         { from: paths.locales, to: paths.buildLocales },
       ],
+    }),
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true,
     }),
   ];
 
--- src/entities/User/model/selectors/roleSelectors.ts
+++ src/entities/User/model/selectors/roleSelectors.ts
@@ -1,6 +1,6 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 import { createSelector } from '@reduxjs/toolkit';
-import { UserRole } from '../types/user';
+import { UserRole } from '../consts/userConsts';
 
 export const getUserRoles = (state: StateSchema) => state.user.authData?.roles;
 
