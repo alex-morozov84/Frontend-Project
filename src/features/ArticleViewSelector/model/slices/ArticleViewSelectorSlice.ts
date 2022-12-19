b9dd10a228350780248a4a49669f2c20ce48a23f
Alexander Morozov <alex.morozov84@yandex.ru>
2022-12-19 22:20:46 +0300

    add lint staged test

--- src/features/ArticleViewSelector/model/slices/ArticleViewSelectorSlice.ts
+++ src/features/ArticleViewSelector/model/slices/ArticleViewSelectorSlice.ts
@@ -2,31 +2,31 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ArticleViewSelectorSchema } from '../types/ArticleViewSelectorSchema';
 
 const initialState: ArticleViewSelectorSchema = {
-    
-}; 
+
+};
 
 export const ArticleViewSelectorSlice = createSlice({
-    name: 'ArticleViewSelector',
-    initialState,
-    reducers: {
-        template: (state, action: PayloadAction<string>) => {
-           
-        },
+  name: 'ArticleViewSelector',
+  initialState,
+  reducers: {
+    template: (state, action: PayloadAction<string>) => {
+
     },
-    // extraReducers: (builder) => {
-    //     builder
-    //         .addCase(, (state) => {
-    //             state.error = undefined;
-    //             state.isLoading = true;
-    //         })
-    //         .addCase(, (state) => {
-    //             state.isLoading = false;
-    //         })
-    //         .addCase(, (state, action) => {
-    //             state.isLoading = false;
-    //             state.error = action.payload;
-    //         });
-    // },
+  },
+  // extraReducers: (builder) => {
+  //     builder
+  //         .addCase(, (state) => {
+  //             state.error = undefined;
+  //             state.isLoading = true;
+  //         })
+  //         .addCase(, (state) => {
+  //             state.isLoading = false;
+  //         })
+  //         .addCase(, (state, action) => {
+  //             state.isLoading = false;
+  //             state.error = action.payload;
+  //         });
+  // },
 });
 
 export const { actions: ArticleViewSelectorActions } = ArticleViewSelectorSlice;
